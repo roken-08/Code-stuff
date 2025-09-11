d05efcc0fc82546a2d19f0bf2482c336f5d1c035
roken <avngravngr@gmail.com>
2025-09-11 22:32:01 +0530

    Add files via upload
    adding the files i use to learn code..would commit quite a few times in the foreseeable future.

---
+++ code.js
@@ -0,0 +1,5 @@
+let btn = document.createElement("button");
+btn.innerText = "click me!";
+btn.style.backgroundColor = "red";
+btn.style.color = "white";
+document.querySelector("body").prepend(btn);
